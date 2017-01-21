faa1b8c91b76f7e7203032b0075a4c88bb7e8873
Hanna Larsen <hanna.c.larsen@gmail.com>
2017-01-21 16:37:04 -0700

    Add project files.

---
+++ CS4150PS2/Ceiling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS4150PS2
+{
+    class Ceiling
+    {
+        
+        static void Main(string[] args)
+        {
+            Ceiling c = new Ceiling();
+            Console.WriteLine(c.UniqueTrees());
+        }
+
+
+        public string UniqueTrees()
+        {
+            string line = "";
+            char[] whitespace = { ' ', '\t' };
+            while((line = Console.ReadLine()) != null)
+            {
+                string[] numbers = line.Split(whitespace);
+
+            }
+            return "";
+        }
+    }
+}
